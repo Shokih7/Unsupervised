94bf338809d5c37fb28879b9ecba8e926c4032bc
Shokih7 <89355956+Shokih7@users.noreply.github.com>
2025-04-27 22:33:43 +0300

    Update getdata.py

--- getdata.py
+++ getdata.py
@@ -1,17 +1,20 @@
-# importing the dataset from the google sheets document.
+import pandas as pd
 
-sheet_url = "https://docs.google.com/spreadsheets/d/1CRS-mgp2RASq4pzmz9rpcXhszWjBMmaX2Ntdk2_RZCc/edit?usp=sharing"
-sheet_id = sheet_url.split('/d/')[1].split('/edit')[0]
-sheet_name = "Dataset"  # Replace with your actual sheet name
+class GetData:
 
-url = f"https://docs.google.com/spreadsheets/d/{sheet_id}/gviz/tq?tqx=out:csv&sheet={sheet_name}"
+    def __init__(self):
+        self.sheet_url = "https://docs.google.com/spreadsheets/d/1CRS-mgp2RASq4pzmz9rpcXhszWjBMmaX2Ntdk2_RZCc/edit?usp=sharing"
+        self.sheet_id = self.sheet_url.split('/d/')[1].split('/edit')[0]
+        self.sheet_name = "Dataset"  # Replace with your actual sheet name
+        self.url = f"https://docs.google.com/spreadsheets/d/{self.sheet_id}/gviz/tq?tqx=out:csv&sheet={self.sheet_name}"
+        self.df = self.create_new_df()
 
-df = pd.read_csv(url)
-# print(df.head())
-
-#Fill Nan
-for col in df.columns:
-    if pd.api.types.is_numeric_dtype(df[col]):
-        df.fillna({col: df[col].mean()}, inplace=True)
-    else:
-        df.fillna({col: df[col].mode()[0]}, inplace=True)
+    def create_new_df(self):
+        df = pd.read_csv(self.url)
+        #Fill Nan
+        for col in df.columns:
+            if pd.api.types.is_numeric_dtype(df[col]):
+                df.fillna({col: df[col].mean()}, inplace=True)
+            else:
+                df.fillna({col: df[col].mode()[0]}, inplace=True)
+        return df
